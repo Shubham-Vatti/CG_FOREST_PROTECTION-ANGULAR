43eb8f6c34c3fdbfe78f9d4e50a9a7245325e813
SHUBHAM KUMAR <shubham.vatti@pcpl.io>
2025-03-28 11:33:35 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CG_FOREST_PROTECTION',
+  webDir: 'www'
+};
+
+export default config;
